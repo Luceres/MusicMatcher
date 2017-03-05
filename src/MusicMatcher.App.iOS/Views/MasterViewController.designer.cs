fdf8873575b1cd810ed264244879bee5f638d0ec
Markus Schiller <markus.schiller@appundmensch.de>
2017-03-05 15:42:50 +0100

    Zelle aufgebaut

--- src/MusicMatcher.App.iOS/Views/MasterViewController.designer.cs
+++ src/MusicMatcher.App.iOS/Views/MasterViewController.designer.cs
@@ -1,18 +1,26 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace MusicMatcher.App.iOS
 {
-    [Register("MasterViewController")]
-    partial class MasterViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("MasterViewController")]
+	partial class MasterViewController
+	{
+		[Outlet]
+		UIKit.UIBarButtonItem _teachButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (_teachButton != null) {
+				_teachButton.Dispose ();
+				_teachButton = null;
+			}
+		}
+	}
 }
--- src/MusicMatcher.App.iOS/Views/SampleCell.designer.cs
+++ src/MusicMatcher.App.iOS/Views/SampleCell.designer.cs
@@ -12,9 +12,39 @@ namespace MusicMatcher.App.iOS
 	[Register ("SampleCell")]
 	partial class SampleCell
 	{
+		[Outlet]
+		UIKit.UIImageView _image { get; set; }
+
+		[Outlet]
+		UIKit.UILabel _info { get; set; }
+
+		[Outlet]
+		UIKit.UILabel _subTitel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel _titel { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (_image != null) {
+				_image.Dispose ();
+				_image = null;
+			}
+
+			if (_titel != null) {
+				_titel.Dispose ();
+				_titel = null;
+			}
+
+			if (_subTitel != null) {
+				_subTitel.Dispose ();
+				_subTitel = null;
+			}
+
+			if (_info != null) {
+				_info.Dispose ();
+				_info = null;
+			}
 		}
 	}
 }
